b29f4910ae6d87163b26d8880a910d86dca61061
Akif Bokth <akifbokth@gmail.com>
2024-11-26 16:51:16 +0000

    Using Deezer API

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 const { isAdmin } = require('../middleware/auth'); // Import admin middleware
 
